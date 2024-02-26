0e6e4d3fa4154b4cec314b7d1cc5ef334ef67f18
Himanshu Chandel <hchandel885@gmail.com>
2024-02-25 22:17:27 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+document.querySelectorAll('input[type="number"]').forEach(inputnumber =>{
+    inputnumber.oninput = () =>{
+        if(inputnumber.ariaValueMax.length > inputnumber.maxlength) inputnumber.value = inputnumber.value.slice(0, inputnumber.maxlength);
+    };
+});
+
+function popup(){
+    alert("Do not share your email password with anyone...Thank YOU!!");
+};
+
+// like heart  animation
+
+
